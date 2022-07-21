6deff5c69ce32d02d4ef199d9005294a30994cb9
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-07-21 14:36:39 +0200

    1. Added Devices category
    And other changes ;)

---
+++ app/src/main/java/com/jvmfrog/ffsettings/DeviceSettingsActivity.java
@@ -0,0 +1,19 @@
+package com.jvmfrog.ffsettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jvmfrog.ffsettings.databinding.ActivityDeviceSettingsBinding;
+
+public class DeviceSettingsActivity extends AppCompatActivity {
+
+    private ActivityDeviceSettingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDeviceSettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
