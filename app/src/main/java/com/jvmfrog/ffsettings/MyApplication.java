98d73c747d0d22bb3c017520182ccccbe80b52f0
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-08-12 22:29:32 +0200

    1. Renamed app 2. Added new languages: - Belarusian - Ukrainian - Turkish

--- app/src/main/java/com/jvmfrog/ffsettings/MyApplication.java
+++ app/src/main/java/com/jvmfrog/ffsettings/MyApplication.java
@@ -112,7 +112,8 @@ public class MyApplication extends Application
     private class AppOpenAdManager {
 
         private static final String LOG_TAG = "AppOpenAdManager";
-        private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/3419835294";
+        private static final String AD_UNIT_ID = "ca-app-pub-4193046598871025/7460189368";
+        private static final String TEST_AD_UNIT_ID = "ca-app-pub-3940256099942544/3419835294";
 
         private AppOpenAd appOpenAd = null;
         private boolean isLoadingAd = false;
