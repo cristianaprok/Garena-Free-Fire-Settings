88eedd9de811cd87d5acbe403e3574b6c69c054c
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2022-08-13 15:33:31 +0200

    Added loading dialog

--- app/src/main/java/com/jvmfrog/ffsettings/ui/fragment/DevicesFragment.java
+++ app/src/main/java/com/jvmfrog/ffsettings/ui/fragment/DevicesFragment.java
@@ -1,18 +1,15 @@
 package com.jvmfrog.ffsettings.ui.fragment;
 
-import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.Query;
@@ -20,12 +17,13 @@ import com.jvmfrog.ffsettings.R;
 import com.jvmfrog.ffsettings.adapter.DevicesAdapter;
 import com.jvmfrog.ffsettings.model.ParamsModel;
 import com.jvmfrog.ffsettings.databinding.FragmentDevicesBinding;
-import com.jvmfrog.ffsettings.utils.AdMobUtil;
 
 public class DevicesFragment extends Fragment {
 
     private FragmentDevicesBinding binding;
     private DevicesAdapter devicesAdapter;
+    private MaterialAlertDialogBuilder builder;
+    private AlertDialog alertDialog;
 
     public DevicesFragment() {
         // Required empty public constructor
@@ -42,6 +40,8 @@ public class DevicesFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentDevicesBinding.inflate(inflater, container, false);
 
+        builder = new MaterialAlertDialogBuilder(getActivity());
+
         Bundle finalBundle = new Bundle();
         finalBundle.putAll(getArguments());
 
@@ -53,6 +53,18 @@ public class DevicesFragment extends Fragment {
 
         Query query = rootRef.collection(finalBundle.getString("device"))
                 .orderBy("device_name", Query.Direction.DESCENDING);
+
+        showLoading();
+        query.get().addOnCompleteListener(task -> {
+            if (task.isComplete() || task.isSuccessful()) {
+
+                if (task.getResult().isEmpty()) {
+                    showLoading();
+                } else {
+                    hideLoading();
+                }
+            }
+        });
 
         FirestoreRecyclerOptions<ParamsModel> options =
                 new FirestoreRecyclerOptions.Builder<ParamsModel>()
@@ -77,5 +89,16 @@ public class DevicesFragment extends Fragment {
         super.onStop();
         if(devicesAdapter != null)
             devicesAdapter.stopListening();
+    }
+
+    private void showLoading() {
+        builder.setView(R.layout.loading_view);
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void hideLoading() {
+        alertDialog.dismiss();
     }
 }
